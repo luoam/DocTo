d8bf63df250a5565c6caf2cb83f98d099a02b984
Toby Allen <tobyallen@toflidium.com>
2019-12-29 17:41:52 +0000

    Add

---
+++ test/php/test1.php
@@ -0,0 +1,13 @@
+<?php
+
+// Run docto
+
+$InputFile = 'C:\\dev\\github\\docto\\test\\InputFiles\\';
+$OutputFile = "C:\\dev\\github\\docto\\test\\GeneratedFiles\\";
+
+$cmd = 'DocTo -f "' . $InputFile . '" -o "' . $OutputFile . '" -t wdFormatPDF -l 10 ' ;
+echo "$cmd \n";
+echo "Excuting....... \n";
+exec($cmd, $out);
+
+print_r($out);
